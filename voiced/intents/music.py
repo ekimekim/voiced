a5f0712992e2ed6326783b55b7a9e45b96d43265
Mike Lang <mikelang3000@gmail.com>
2021-06-06 12:08:36 +1000

    Implement music control, in a hacky way for now
    Volume up/down seem to only apply once instead of 3 times?

--- voiced/intents/music.py
+++ voiced/intents/music.py
@@ -1,9 +1,33 @@
 
 import logging
+import socket
+from contextlib import closing
 
+from .. import sounds
 from ..intent import intent
+
+
+CONTROL_SOCK_PATH = "/tmp/awp.sock"
+
+
+def write_control(char):
+	with closing(socket.socket(socket.AF_UNIX)) as sock:
+		sock.connect(CONTROL_SOCK_PATH)
+		sock.sendall(char)
 
 
 @intent("music")
 def music(text, command, volume=None):
-	logging.info("STUB: music intent: {}, volume={}".format(command, volume))
+	char = {
+		"favourite": "f",
+		"skip": "q",
+		"next": "\n",
+		"pause": " ",
+		"unpause": " ",
+		"volume up": "***",
+		"volume down": "///",
+	}.get(command)
+	if char is None:
+		logging.warning("Unimplemented command: {}".format(command))
+		return sounds.ERROR
+	write_control(char)
